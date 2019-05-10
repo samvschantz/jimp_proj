0cfb12eb5051152f96321ee2f411490b8e46e182
Samuel Schantz <Samuel Schantz@tbadigital.local>
2019-05-10 15:23:49 -0700

    adding input and submit handlers as well as conditional display

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -4,20 +4,35 @@ import '../../App.css';
 
 
 class Input extends Component {
+    state = {
+    	textEntered: false,
+    	text: ''
+    }
 
     inputHandler = (e) =>{
+    	console.log(e)
+    	console.log(e.value);
+    };
 
-    }
+    clickHandler = () =>{
+    	console.log('hitting this.');
+    	this.setState({ textEntered: true });
+    };
 
 	render(){
 		return (
 		    <div className="App">
+		    {this.state.textEntered ? <Result text={ this.state.text } />:
+		    <div>
 		      <header className="App-header">
 		        <p>
-		          What do youuuuu want your hat to say? 
+		          What do you want your hat to say? 
 		        </p>
 		      </header>
-		      <input type='text' name='hatText' onInput={this.inputHandler}/>
+		      <input type='text' name='hatText' onInput={this.inputHandler} />
+		      <button onClick={this.clickHandler}>Submit</button>
+		    </div>
+		    }
 		    </div>
 		)
 	}
