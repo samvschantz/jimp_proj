92d19d5fca7e5290c160896c763d0f3aa729d7f7
Samuel Schantz <Samuel Schantz@tbadigital.local>
2019-05-10 16:08:22 -0700

    adding jimp functions

--- src/components/result/index.js
+++ src/components/result/index.js
@@ -4,8 +4,26 @@ import '../../App.css';
 
 
 function Result(props){
+
 	useEffect(() => {
 		console.log(props.text);
+		let fileName = '../../../assets/red_hat.jpg';
+		console.log(fileName);
+		let imageCaption = 'Image caption';
+		let loadedImage;
+
+		Jimp.read(fileName)
+		    .then(function (image) {
+		        loadedImage = image;
+		        return Jimp.loadFont(Jimp.FONT_SANS_16_BLACK);
+		    })
+		    .then(function (font) {
+		        loadedImage.print(font, 10, 10, imageCaption)
+		                   .write(fileName);
+		    })
+		    .catch(function (err) {
+		        console.error(err);
+		    });
 	})
 
 	return(
