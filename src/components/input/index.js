63226bfe73a04b0e587a95f54db14a7f60d7df97
Samuel Schantz <Samuel Schantz@tbadigital.local>
2019-05-10 15:10:20 -0700

    reconfigured folder structure for components

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -1,1 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Jimp from 'jimp';
+import '../../App.css';
+
+
+class Input extends Component {
+
+    inputHandler = (e) =>{
+
+    }
+
+	render(){
+		return (
+		    <div className="App">
+		      <header className="App-header">
+		        <p>
+		          What do youuuuu want your hat to say? 
+		        </p>
+		      </header>
+		      <input type='text' name='hatText' onInput={this.inputHandler}/>
+		    </div>
+		)
+	}
+}
+
+export default Input;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Input from '../components/Input'
+import Input from './components/Input'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Input />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
